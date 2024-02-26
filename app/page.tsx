afdde4b9063d0bed7fea528d6c10e0df73f082c3
Merv <martinrtownley@gmail.com>
2024-02-26 11:18:30 +0000

    remove swiper imports

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,6 @@ import Features from "@/components/Features";
 import GetApp from "@/components/GetApp";
 import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
-import SwipeGallery from "@/components/SwipeGallery";
-
-import "swiper/swiper-bundle.css";
-import { register } from "swiper/element/bundle";
-
-register();
 
 export default function Home() {
   return (
